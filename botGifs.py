6f3966372404a55e6bd6a7e2e3fe08fac0ae974d
Smay Askling <>
2021-05-15 07:22:11 +0000

    Basic start

---
+++ botGifs.py
@@ -0,0 +1,25 @@
+hugGif = [
+  "https://c.tenor.com/9e1aE_xBLCsAAAAM/anime-hug.gif",
+  "https://c.tenor.com/c8M8yU1q6c4AAAAM/hug-anime.gif",
+  "https://c.tenor.com/jQ0FcfbsXqIAAAAM/hug-anime.gif",
+  "https://c.tenor.com/3ergzHiRIBwAAAAM/hug-cuddle.gif",
+  "https://c.tenor.com/5u1n4SYgd3AAAAAM/cuddle-nuzzle.gif",
+  "https://c.tenor.com/zirc8LTWVUkAAAAM/hug-anime.gif",
+  "https://c.tenor.com/UhcyGsGpLNIAAAAM/hug-anime.gif",
+  "https://c.tenor.com/JTqXUbfSSkYAAAAM/anime-bed.gif,",
+  "https://c.tenor.com/VqazOH8fQ8gAAAAM/anime-hug.gif",
+  "https://c.tenor.com/xgVPw2QK5n8AAAAM/sakura-quest-anime.gif",
+  "https://c.tenor.com/o1jezAk92FUAAAAM/sound-euphonium-hug.gif",
+  "https://c.tenor.com/X5nBTYuoKpoAAAAM/anime-cheeks.gif",
+  "https://c.tenor.com/2bWwi8DhDsAAAAAM/hugs-and-love.gif",
+]
+
+selfHugGif = [
+  "https://c.tenor.com/MkEg8FyWR38AAAAM/sad-cute.gif",
+  "https://c.tenor.com/RRG7pXMcSloAAAAM/sad-anime.gif",
+  "https://c.tenor.com/NYUvDH9UoM8AAAAM/anime-cute.gif",
+  "https://c.tenor.com/2bHv9LeDxYIAAAAM/sailor-moon-black-lady.gif",
+  "https://c.tenor.com/OK2cI827baIAAAAM/osomatsu-osomatsu-san.gif",
+  "https://c.tenor.com/MctOwe2LKgQAAAAM/franziska-alone.gif",
+  "https://c.tenor.com/fWjs4Isxni4AAAAM/anime-anime-sad.gif",
+]
--- main.py
+++ main.py
@@ -1,1 +1,43 @@
 #Bot
+import discord
+import botGifs
+import random
+
+TOKEN = "ODQyOTkwODM4NDg1MDkwMzA2.YJ9WZQ.DnjiA1kxmS4YvErwNdWy7Vsfho0"
+
+client = discord.Client()
+
+prefix = "eli"
+
+@client.event
+async def on_ready():
+  print("I love coffee")
+  
+  async for guild in client.fetch_guilds(limit=150):
+    print(guild.name)
+
+@client.event
+async def on_message(message):
+  if message.author.bot:
+    return
+  elif message.content.lower().startswith(prefix + " cutie"):
+    await message.channel.send("ur a cutie 2 ;3")
+  elif message.content.lower().startswith(prefix + " hug"):
+    messageList = message.content.lower().split()
+    if len(messageList) >= 3:
+      await message.channel.send(f"{message.author.name} is hugging {messageList[2]} {randoHug()}")
+    else:
+      await message.channel.send(f"{message.author.name} wants a hug... {randoSelfHug()}")
+
+
+
+def randoHug():
+  return random.choice(botGifs.hugGif)
+
+def randoSelfHug():
+  return random.choice(botGifs.selfHugGif)
+
+#<@1234567890>
+
+
+client.run(TOKEN)
