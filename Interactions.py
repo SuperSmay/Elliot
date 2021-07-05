ddfba92af1d69f165a358e0a67ac11bd906740f9
Smay <samaskling@gmail.com>
2021-07-05 16:58:03 -0500

    Update gitignore

--- Interactions.py
+++
@@ -1,6 +0,0 @@
-class Hug:
-    def solo(userName, otherNames= None):
-        return f"{userName} wants a hug..."
-
-    def multi(userName, otherNames):
-        return f"{userName} is hugging {otherNames.join(' ')}"
