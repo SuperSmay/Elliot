9e0405656459e2ef54b851eae1ceb1bbaabcce7e
Sam Askling <samaskling@gmail.com>
2022-06-19 21:04:42 -0400

    Basic level system works:tm:

--- Levels.py
+++ Levels.py
@@ -14,6 +14,36 @@ class LevelManager(Leaderboard.LeaderboardData):
         self.defaults = {"message_count" : 0, "voice_chat_time": 0, "xp": 0, "last_xp_message_time": 0}
         self.default_column = "xp"
     
+    def get_xp(self, member, leaderboard=None):
+        leaderboard = self.get_leaderboard(member) if leaderboard is None else leaderboard
+        return self.get_member_score(member, leaderboard, column_name="xp")
+    
+    def change_xp(self, member, change, leaderboard=None):
+        leaderboard = self.get_leaderboard(member) if leaderboard is None else leaderboard
+        xp = self.get_xp(member, leaderboard)
+        xp += change
+        return self.set_score(member, xp, column_name="xp")
+
+    def get_message_count(self, member, leaderboard=None):
+        leaderboard = self.get_leaderboard(member) if leaderboard is None else leaderboard
+        return self.get_member_score(member, leaderboard, column_name="message_count")
+    
+    def change_message_count(self, member, change, leaderboard=None):
+        leaderboard = self.get_leaderboard(member) if leaderboard is None else leaderboard
+        message_count = self.get_message_count(member, leaderboard)
+        message_count += change
+        return self.set_score(member, message_count, column_name="message_count")
+
+    def get_voice_chat_time(self, member, leaderboard=None):
+        leaderboard = self.get_leaderboard(member) if leaderboard is None else leaderboard
+        return self.get_member_score(member, leaderboard, column_name="voice_chat_time")
+    
+    def change_voice_chat_time(self, member, change, leaderboard=None):
+        leaderboard = self.get_leaderboard(member) if leaderboard is None else leaderboard
+        voice_chat_time = self.get_voice_chat_time(member, leaderboard)
+        voice_chat_time += change
+        return self.set_score(member, voice_chat_time, column_name="voice_chat_time")
+    
 class Levels(commands.Cog):
     def __init__(self) -> None:
         ...
@@ -30,8 +60,8 @@ class Levels(commands.Cog):
             return
         
         level_manager.set_score(message.author, message_time, column_name="last_xp_message_time")
+        
 
-        message_count = level_manager.get_member_score(message.author, leaderboard, column_name="message_count")
-        message_count += 1
+        level_manager.change_message_count(message.author, 1, leaderboard=leaderboard)
 
-        level_manager.set_score(message.author, message_count, column_name="message_count")
+        level_manager.change_xp(message.author, 20, leaderboard=leaderboard)
