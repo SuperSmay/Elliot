5b7bbcc6efe73cf65e1a486ff8cad9c73f5e0979
Sam Askling <samaskling@gmail.com>
2022-06-21 18:12:20 -0400

    Scuffed vc xp

--- Levels.py
+++ Levels.py
@@ -1,12 +1,21 @@
 import discord
 from discord.commands import Option, OptionChoice
-from discord.ext import commands
+from discord.ext import commands, tasks
 import datetime
+import logging
 
-from GlobalVariables import bot
+from GlobalVariables import bot, on_log
+from Statistics import log_event
 
 # import DBManager
 import Leaderboard
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addFilter(on_log)
+
+MESSAGE_XP = 20
+VOICE_XP = 5
 
 class LevelManager(Leaderboard.LeaderboardData):
     def __init__(self):
@@ -76,29 +85,76 @@ class LevelManager(Leaderboard.LeaderboardData):
     
 class Levels(commands.Cog):
     def __init__(self) -> None:
-        ...
+        self.levels_voice_chat_loop.start()
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+
         if message.author.bot: 
             return
+        
         level_manager = LevelManager()
         leaderboard = level_manager.get_leaderboard(message.author)
 
         last_xp_time = level_manager.get_member_score(message.author, leaderboard, column_name="last_xp_message_time")
         message_time = int(datetime.datetime.timestamp(message.created_at))
         
+        # Ignore if message was within a minute of the last time XP was given
         if (message_time - last_xp_time) < 60:
             return
-        
+
+        # Set the last time a message gave this user XP
         level_manager.set_score(message.author, message_time, column_name="last_xp_message_time")
-
+        # Add the message count
         level_manager.change_message_count(message.author, 1, leaderboard=leaderboard)
+        # Add the XP
+        level_manager.change_xp(message.author, MESSAGE_XP, leaderboard=leaderboard)
 
-        level_manager.change_xp(message.author, 20, leaderboard=leaderboard)
+    @tasks.loop(minutes=1)
+    async def levels_voice_chat_loop(self):
+        logger.info("Checking voice chats for xp")
+
+        for guild in bot.guilds:
+
+            for category in guild.categories:
+
+                for channel in category.voice_channels:
+                    
+
+                    logger.info(channel.name)
+
+                    level_manager = LevelManager()
+
+                    # if channel.type != discord.ChannelType.voice:
+                    #     return
+
+                    voice_states: dict[int, discord.VoiceState] = channel.voice_states
+
+                    # if len(voice_states) < 2:
+                    #     return 
+
+                    for member_id, voice_state in voice_states.items():
+                        # Ignore AFK users and deafened users (They aren't really participating so...)
+                        if voice_state.afk or voice_state.self_deaf or voice_state.deaf:
+                            continue
+
+                        member = await guild.fetch_member(member_id)
+
+                        leaderboard = level_manager.get_leaderboard(member)
+
+                        level_manager.change_voice_chat_time(member, 1, leaderboard)
+                        level_manager.change_xp(member, VOICE_XP)
+
+    @levels_voice_chat_loop.before_loop
+    async def before_voice_loop(self):
+        logger.info("Starting Voice XP Loop...")
+
+
 
     @commands.slash_command(name="levels", description="Shows the level leaderboard")
     async def levels_slash(self, ctx, sort:Option(str, description='Sort levels leaderboard', choices=[OptionChoice('Total XP', 'xp'), OptionChoice('Minutes messaged', 'messages'), OptionChoice('Minutes in VC', 'vc')], required=False, default='xp'), page_number:Option(int, description='Page number', required=False, default=1)):
+        log_event('slash_command', ctx=ctx)
+        log_event('leaderboard_command', ctx=ctx)
         level_manager = LevelManager()
         page_index = max(0, page_number - 1)
         if sort == 'messages': sort = 'message_count'
@@ -109,6 +165,8 @@ class Levels(commands.Cog):
 
     @commands.command(name="levels", description="Shows the level leaderboard")
     async def levels_prefix(self, ctx, sort='xp', page_number=1):
+        log_event('prefix_command', ctx=ctx)
+        log_event('leaderboard_command', ctx=ctx)
         level_manager = LevelManager()
         page_index = max(0, page_number - 1)
         if sort == 'messages' or sort == 'message' or sort == 'm': 
